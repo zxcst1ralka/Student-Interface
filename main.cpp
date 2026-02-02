15852b31139a251f4c55b14feff42b0232c47394
dogerror174-lang <dogerror174@gmail.com>
2026-02-02 16:11:23 +0300

    last_commit

--- main.cpp
+++ main.cpp
@@ -29,3 +29,71 @@ void gotoXY(int x, int y) {
     COORD coord = { (SHORT)x, (SHORT)y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+void mainMenu() {
+    clearScreen();
+    setColor(11);
+
+    cout << "=========================\n";
+    cout << "   СИСТЕМА СТУДЕНТОВ\n";
+    cout << "=========================\n";
+    cout << "1. Просмотр студентов\n";
+    cout << "2. Выставить оценки\n";
+    cout << "3. Выход\n";
+    cout << "Выберите пункт: ";
+}
+
+int main() {
+    SetConsoleTitleA("Система управления студентами");
+
+    int choice;
+
+    while (true) {
+        mainMenu();
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            viewStudents();
+            break;
+        case 2:
+            setGrades();
+            break;
+        case 3:
+            return 0;
+        default:
+            cout << "Неверный ввод!\n";
+            Sleep(1000);
+        }
+    }
+}
+
+void viewStudents() {
+    int index = 0;
+    char key;
+
+    while (true) {
+        clearScreen();
+        setColor(14);
+
+        cout << "===== ПРОСМОТР СТУДЕНТОВ =====\n\n";
+        cout << "Студент: " << students[index].name << "\n";
+        cout << "Возраст: " << students[index].age << "\n";
+        cout << "Оценка: " << students[index].grade << "\n\n";
+
+        setColor(10);
+        cout << "[A] Назад  [D] Вперед  [Q] Выйти\n";
+        setColor(7);
+
+        key = getchar();
+
+        if (key == 'a' || key == 'A') {
+            if (index > 0) index--;
+        }
+        else if (key == 'd' || key == 'D') {
+            if (index < students.size() - 1) index++;
+        }
+        else if (key == 'q' || key == 'Q') {
+            break;
+        }
+    }
+}
