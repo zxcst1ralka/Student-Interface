2dc12bafc85d875ba2a3817801165219df271eae
zxcst1ralka <faithfuk@yandex.ru>
2026-02-02 16:06:36 +0300

    core

--- main.cpp
+++ main.cpp
@@ -12,8 +12,58 @@ struct Student {
 };
 
 vector<Student> students = {
-    {"���� ������", 18, 0},
-    {"���� ��������", 19, 0},
-    {"������� ��������", 20, 0},
-    {"����� �������", 18, 0}
+    {"���� ������", 18, 0},
+    {"���� ��������", 19, 0},
+    {"������� ��������", 20, 0},
+    {"����� �������", 18, 0}
 };
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void clearScreen() {
+    system("cls");
+}
+
+void gotoXY(int x, int y) {
+    COORD coord = { (SHORT)x, (SHORT)y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+void mainMenu() {
+    clearScreen();
+    setColor(11);
+
+    cout << "=========================\n";
+    cout << "   СИСТЕМА СТУДЕНТОВ\n";
+    cout << "=========================\n";
+    cout << "1. Просмотр студентов\n";
+    cout << "2. Выставить оценки\n";
+    cout << "3. Выход\n";
+    cout << "Выберите пункт: ";
+}
+
+int main() {
+    SetConsoleTitleA("Система управления студентами");
+
+    int choice;
+
+    while (true) {
+        mainMenu();
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            viewStudents();
+            break;
+        case 2:
+            setGrades();
+            break;
+        case 3:
+            return 0;
+        default:
+            cout << "Неверный ввод!\n";
+            Sleep(1000);
+        }
+    }
+}
+
